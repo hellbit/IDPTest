4ac80c4bb494ab04cef3a95ebd56e4c8faf1f455
Alexander Vdovichenko <vdovichenko.a.a@gmail.com>
2013-12-25 17:02:25 +0200

    add project And write test for IDPModel

---
+++ IDPTest/src/Model/IDPTestModel.m
@@ -0,0 +1,23 @@
+//
+//  IDPTestModel.m
+//  IDPTest
+//
+//  Created by HeLLBiT on 11/8/13.
+//  Copyright (c) 2013 Alexander. All rights reserved.
+//
+
+#import "IDPTestModel.h"
+
+@implementation IDPTestModel
+
+#pragma mark -
+#pragma mark Public
+
+- (BOOL)load {
+    if (![super load]) {
+        return NO;
+    }
+    return YES;
+}
+
+@end
